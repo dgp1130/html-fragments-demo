c4213f38d8480cc2daa912a9a298e273fff49428
Doug Parker <dgp1130@users.noreply.github.com>
2022-08-04 21:43:37 -0700

    Don't reload the `<em-bed />` tag if it gets detached and reattached to the DOM.

--- client/em-bed.ts
+++ client/em-bed.ts
@@ -1,10 +1,17 @@
 import { parseDomFragment } from './dom.js';
 
 class Embed extends HTMLElement {
+    private loaded = false;
+
     public connectedCallback(): void {
+        if (this.loaded) return;
+
         const src = this.getAttribute('src');
         if (!src) throw new Error(`<em-bed /> requires a \`src\` attribute.`);
 
+        // Mark the component has loaded to avoid double-requesting the `src`
+        // if this element is detached and reattached to the DOM.
+        this.loaded = true;
         (async () => {
             const res = await fetch(src);
             const template = await parseDomFragment(res);
